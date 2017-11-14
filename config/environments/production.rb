f4064edd1bcd8cc3bd2199f1260f28bf67108327
Elliott Potter <elliott@shipt.com>
2017-11-14 13:44:03 -0600

    Try again to get google fonts into production 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,11 +30,11 @@ Rails.application.configure do
   # Disable minification since it adds a *huge* amount of time to precompile.
   # Anyway, gzip alone gets us about 70% of the benefits of minify+gzip.
   config.assets.js_compressor = false
-  config.assets.css_compressor = false
+  # config.assets.css_compressor = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
